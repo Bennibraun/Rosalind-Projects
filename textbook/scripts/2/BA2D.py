0e572e54bb8d40141e66a00809fd4800f4490ab6
Ben Braun <benjaminbraun@live.com>
2023-08-05 13:29:16 -0400

    finally solved greedy motif search

--- textbook/scripts/2/BA2D.py
+++ textbook/scripts/2/BA2D.py
@@ -17,41 +17,37 @@ if __name__ == '__main__':
 			dna = [line.strip() for line in f.readlines()]
 		
 
-
 def probability(kmer,profile):
-	#profile is a 4xk matrix
-	mat_rows = ['A','C','G','T']
-	k_row = [mat_rows.index(kmer[0])]
-	prob_n = profile[k_row[0]][0]
-	if prob_n == 0:
-		return 0
-	elif len(kmer) == 1:
-		return prob_n
-	else:
-		return prob_n * probability(kmer[1:],[profile[i][1:] for i in range(4)])
+    #profile is a 4xk matrix
+    mat_rows = ['A','C','G','T']
+    prob_n = 1
+    for i in range(len(kmer)):
+        prob_n *= profile[mat_rows.index(kmer[i])][i]
+    return prob_n
 
+def profile_most_probable_kmer(dna,profile,k):
+    #profile is a 4xk matrix
+    max_prob = 0
+    max_kmer = ''
+    for i in range(len(dna)-k+1):
+        kmer = dna[i:i+k]
+        prob = probability(kmer,profile)
+        if prob > max_prob or max_kmer == '':
+            max_prob = prob
+            max_kmer = kmer
+    return max_kmer
 
-def profile_most_probable_kmer(dna,profile):
-	#profile is a 4xk matrix
-	k = len(profile[0])
-	max_prob = 0
-	for kmer in [dna[i:i+k] for i in range(len(dna)-k+1)]:
-		prob = probability(kmer,profile)
-		if prob > max_prob:
-			max_prob = prob
-			max_kmer = kmer
-	return max_kmer
-
-def make_profile(dna,k,n):
-	mat_rows = ['A','C','G','T']
-	profile = [[],[],[],[]]
-	for i in range(n,k+n):
-		kth_nucs = [dna[j][i] for j in range(len(dna))]
-		counts = [kth_nucs.count(nuc) for nuc in mat_rows]
-		probs = [count/sum(counts) for count in counts]
-		for j in range(len(probs)):
-			profile[j].append(probs[j])
-	return profile
+def make_profile(dna,k,start):
+    mat_rows = ['A','C','G','T']
+    profile = [[],[],[],[]]
+    for i in range(start,start+k):
+        kth_nucs = [dna[j][i] for j in range(len(dna)) if len(dna[j]) > i]
+        counts = [kth_nucs.count(nuc) for nuc in mat_rows]
+        sum_counts = sum(counts)
+        probs = [count/sum_counts if sum_counts != 0 else 0 for count in counts]
+        for j in range(len(probs)):
+            profile[j].append(probs[j])
+    return profile
 
 def get_motifs(dna,k,n):
 	return [dna_i[n:n+k] for dna_i in dna]
@@ -74,23 +70,20 @@ def score(motifs):
 	return score
 
 def greedy_motif_search(dna,k,t):
-	best_motifs = get_motifs(dna,k,0)
-	mat_rows = ['A','C','G','T']
-	for motif in [dna[0][j:j+k] for j in range(len(dna[0])-k+1)]:
-		print(motif)
-		motif1 = motif
-		motifs = [motif1]
-		for i in range(1,t):
-			motif_i = get_motifs(dna,k,i)
-			print('motif_i:',motif_i)
-			profile = make_profile(motif_i,k,0)
-			motifs.append(profile_most_probable_kmer(dna[i],profile))
-			print('motifs: ',motifs)
-		if score(motifs) < score(best_motifs):
-			best_motifs = motifs
-			print('best motif: ',best_motifs)
-	return best_motifs
+    best_motifs = [dna_i[0:k] for dna_i in dna]
+    for i in range(len(dna[0])-k+1):
+        motif = dna[0][i:i+k]
+        motifs = [motif]
+        for j in range(1,t):
+            profile = make_profile(motifs,k,0)
+            motifs.append(profile_most_probable_kmer(dna[j],profile,k))
+        if score(motifs) < score(best_motifs):
+            best_motifs = motifs
+    return best_motifs
 
+
+# print(score(get_motifs(['GGCGTTCAGGCA','AAGAATCAGTCA','CAAGGAGTTCGC','CACGTCAATCAC','CAATAATATTCG'],3,0)))
+# print(make_profile(['GGCGTTCAGGCA','AAGAATCAGTCA','CAAGGAGTTCGC','CACGTCAATCAC','CAATAATATTCG'],3,0))
 
 # 3 5
 # GGCGTTCAGGCA
@@ -98,10 +91,11 @@ def greedy_motif_search(dna,k,t):
 # CAAGGAGTTCGC
 # CACGTCAATCAC
 # CAATAATATTCG
-print('\n'.join(greedy_motif_search(['GGCGTTCAGGCA','AAGAATCAGTCA','CAAGGAGTTCGC','CACGTCAATCAC','CAATAATATTCG'],3,5)))
+# print('\n'.join(greedy_motif_search(['GGCGTTCAGGCA','AAGAATCAGTCA','CAAGGAGTTCGC','CACGTCAATCAC','CAATAATATTCG'],3,5)))
 
 # 3 4
 # GCCCAA GGCCTG AACCTA TTCCTT
 # print(greedy_motif_search(['GCCCAA','GGCCTG','AACCTA','TTCCTT'],3,4))
+# print(greedy_motif_search_online(['GGCGTTCAGGCA','AAGAATCAGTCA','CAAGGAGTTCGC','CACGTCAATCAC','CAATAATATTCG'],3))
 
-# print('\n'.join(greedy_motif_search(dna,k,t)))
+print('\n'.join(greedy_motif_search(dna,k,t)))
